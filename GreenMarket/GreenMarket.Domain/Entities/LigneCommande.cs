f47319f3039b65636b766ddd6dfebdbed0e0d330
Julien LEGRAND <j.legrand@cobetix.com>
2026-04-21 16:34:52 +0200

    Ajout d'interfaces

--- GreenMarket/GreenMarket.Domain/Entities/LigneCommande.cs
+++ GreenMarket/GreenMarket.Domain/Entities/LigneCommande.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GreenMarket.Domain.Entities;
@@ -5,22 +6,23 @@ namespace GreenMarket.Domain.Entities;
 public class LigneCommande
 {
     public int CommandeId { get; set; }
-
     [ForeignKey(nameof(CommandeId))]
     public virtual Commande Commande { get; set; } = null!;
 
-    public int ProducteurId { get; set; }
-
-    [ForeignKey(nameof(ProducteurId))]
-    public virtual Producteur Producteur { get; set; } = null!;
-
     public int ProduitId { get; set; }
-
     [ForeignKey(nameof(ProduitId))]
     public virtual Produit Produit { get; set; } = null!;
 
+    public int ProducteurId { get; set; }
+    [ForeignKey(nameof(ProducteurId))]
+    public virtual Producteur Producteur { get; set; } = null!;
+
+    [Range(1, int.MaxValue)]
     public int Quantite { get; set; }
 
     [Column(TypeName = "numeric(10,2)")]
     public decimal PrixUnitaire { get; set; }
+
+    [NotMapped]
+    public decimal SousTotal => PrixUnitaire * Quantite;
 }
